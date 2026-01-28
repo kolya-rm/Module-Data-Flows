cb24e429f907fc84fc623e94e8c29599440bfdd8
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-28 10:37:33 +0100

    Sprint 1. part prep:   - js code moved in script.js file.

--- Sprint-1/prep/script.js
+++ Sprint-1/prep/script.js
@@ -5,7 +5,23 @@ const film = {
   certificate: "15",
   duration: 112,
 };
+
+const card = document.createElement("section");
+
 const filmTitle = document.createElement("h3");
 filmTitle.textContent = film.title;
-document.body.append(filmTitle);
-console.log(filmTitle);
+card.append(filmTitle);
+
+const director = document.createElement("p");
+director.textContent = `Director: ${film.director}`;
+card.append(director);
+
+const duration = document.createElement("time");
+duration.textContent = `${film.duration} minutes`;
+card.append(duration);
+
+const certificate = document.createElement("data");
+certificate.textContent = `Certificate: ${film.certificate}`;
+card.append(certificate);
+
+document.body.append(card);
