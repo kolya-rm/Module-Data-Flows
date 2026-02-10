ae4ae972e915513fed3f1ff2aa405903143b0aa6
Nikolai Romanov <kolya.rm@gmail.com>
2026-02-10 10:53:11 +0100

    Added Sprint 3 part prep files.

---
+++ Sprint-3/prep/script.js
@@ -0,0 +1,51 @@
+const state = {
+  films: [
+    {
+      title: "Killing of Flower Moon",
+      director: "Martin Scorsese",
+      times: ["15:35"],
+      certificate: "15",
+      duration: 112,
+    },
+    {
+      title: "Typist Artist Pirate King",
+      director: "Carol Morley",
+      times: ["15:00", "20:00"],
+      certificate: "12A",
+      duration: 108,
+    },
+  ],
+  searchTerm: "pirate",
+};
+
+const template = document.getElementById("film-card");
+
+function createFilmCard({title, director, duration, certificate}) {
+  const card = template.content.cloneNode(true);
+
+  card.querySelector("h3").textContent = title;
+  card.querySelector("[data-director]").textContent = `Director  ${director}`;
+  card.querySelector("time").textContent = `${duration} minutes`;
+  card.querySelector("[data-certificate]").textContent = `Certificate: ${certificate}`;
+
+  return card;
+}
+
+function render() {
+  const filteredFilms = state.films.filter(
+    (film) => film.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+  );
+  const filmCards = filteredFilms.map(createFilmCard);
+  const filmsSection = document.getElementById("films");
+
+  filmsSection.textContent = "";
+  filmsSection.append(...filmCards);
+}
+
+const searchBox = document.getElementById("q");
+searchBox.addEventListener("input", handleSearchInput);
+
+function handleSearchInput(event) {
+  state.searchTerm = event.target.value;
+  render();
+}
